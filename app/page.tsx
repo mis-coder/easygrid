6795203bd8765006bb5e032fbd0c3a243844e9f1
Antra Verma <antraverma99@gmail.com>
2024-10-15 20:43:05 +0530

    updated home page content and ui

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,18 @@ export default function Home() {
     <div className="h-screen w-full flex items-center justify-center">
       <div className="max-w-screen-md bg-white flex flex-col items-center rounded-3xl p-20 shadow-[0px_5px_10px_rgba(0,0,0,0.06)]">
         <Logo />
-        <h1 className="font-bold text-6xl text-grey">EasyGrid</h1>
-        <p className="italic">Made learning CSS Grid easier!</p>
-        <p className="my-4">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five.
+        <h1 className="font-bold text-5xl text-grey leading-normal">
+          EasyGrid
+        </h1>
+        <p className="italic text-xs">Made learning CSS Grid easier!</p>
+        <p className="my-4 text-center text-sm pt-5">
+          Welcome to EasyGrid!
+          <br />
+          EasyGrid is a free course designed to help you learn and master CSS
+          Grid. Each lesson is paired with interactive puzzles and challenges,
+          allowing you to practice and apply what you’ve learned in real time.
         </p>
-        <button className="bg-green-500 rounded-3xl py-2 px-10 text-white">
+        <button className="bg-green-500 rounded-3xl py-2 px-10 text-white text-sm font-semibold mt-5">
           Let's Begin!
         </button>
       </div>
